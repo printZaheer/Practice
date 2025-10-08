52890e6b2e1e4bbd4ce20fba45a9af4143c9899d
printZaheer <232248698+printZaheer@users.noreply.github.com>
2025-10-08 14:30:06 +0500

    Rock-Paper-Scissors game between two players

--- Rock_paper_Scissors-game.c
+++ Rock_paper_Scissors-game.c
@@ -1,16 +1,18 @@
-#include<stdio.h>
-int main (){
-  int player1_choice,player2_choice;
+#include <stdio.h>
+int main()
+{
+    int player1_choice, player2_choice;
     printf("Welcome to Rock-Paper-Scissors(2 Players) Game.\n");
     printf("1-Rock\n");
     printf("2-Paper\n");
     printf("3-Scissors\n");
     printf("Player 1 , enter your choice: ");
-    scanf("%d",&player1_choice);
+    scanf("%d", &player1_choice);
     printf("Player 2 , enter your choice: ");
     scanf("%d", &player2_choice);
- 
-    if (player1_choice==1){
+
+    if (player1_choice == 1)
+    {
         printf("\nplayer 1 choose: Rock");
     }
     else if (player1_choice == 2)
@@ -42,38 +44,38 @@ int main (){
         printf(" invalid! \n");
     }
 
-        if (player1_choice == player2_choice)
-        {
-            printf("the game is  tie.");
-        }
-        else if (player1_choice == 1 && player2_choice == 2)
-        {
-            printf("player2 wins the game.");
-        }
-        else if (player1_choice == 2 && player2_choice == 1)
-        {
-            printf("player1 wins the game.");
-        }
-        else if (player1_choice == 2 && player2_choice == 3)
-        {
-            printf("player2 wins the game.");
-        }
-        else if (player1_choice == 3 && player2_choice == 2)
-        {
-            printf("player1 wins the game.");
-        }
-        else if (player1_choice == 1 && player2_choice == 3)
-        {
-            printf("player1 wins the game.");
-        }
-        else if (player1_choice == 3 && player2_choice == 1)
-        {
-            printf("player2 wins the game.");
-        }
-        else
-        {
-            printf("You enter invalid choice.");
-        }
+    if (player1_choice == player2_choice)
+    {
+        printf("the game is  tie.");
+    }
+    else if (player1_choice == 1 && player2_choice == 2)
+    {
+        printf("player2 wins the game.");
+    }
+    else if (player1_choice == 2 && player2_choice == 1)
+    {
+        printf("player1 wins the game.");
+    }
+    else if (player1_choice == 2 && player2_choice == 3)
+    {
+        printf("player2 wins the game.");
+    }
+    else if (player1_choice == 3 && player2_choice == 2)
+    {
+        printf("player1 wins the game.");
+    }
+    else if (player1_choice == 1 && player2_choice == 3)
+    {
+        printf("player1 wins the game.");
+    }
+    else if (player1_choice == 3 && player2_choice == 1)
+    {
+        printf("player2 wins the game.");
+    }
+    else
+    {
+        printf("You enter invalid choice.");
+    }
 
     return 0;
 }
