0c9e11c990ae1992bfbeef0a6226ec6662a9321b
printZaheer <232248698+printZaheer@users.noreply.github.com>
2025-10-21 23:47:01 +0500

    Making pattern of Pyramid from Right Sides

---
+++ Pyramid_from_Right_sides.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+
+    for (int x = 1; x <= 5; x++)
+    {
+        for (int z = 4; z >= x; z--)
+        {
+            printf(" ");
+        }
+        for (int y = 1; y <= x; y++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    for (int x = 1; x <= 5; x++)
+    {
+        for (int z = 1; z < x; z++)
+        {
+            printf(" ");
+        }
+        for (int y = 5; y >= x; y--)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
